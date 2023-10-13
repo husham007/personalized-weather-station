3c16b7ec5b20a6e89c90e9ea7a208644d2a15e63
ad.hanif <ad.hanif@outlook.com>
2023-10-13 13:22:53 +0200

    Navbar added

--- pws-ui/src/App.jsx
+++ pws-ui/src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
-import SignIn from "./components/molecules/SignIn.jsx";
-
 import LoginPage from "./components/molecules/LoginPage.jsx";
 import SignUpPage from "./components/molecules/SignUpPage.jsx";
+import Navbar from "./components/molecules/Navbar.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <h1>Vite + React</h1>
-      </div>
-      {/* <SignIn /> */}
+      <Navbar />
       <LoginPage />
       <SignUpPage />
     </>
