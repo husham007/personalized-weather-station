76f954984c1e5ef0eeb5d8d57c6da311bf85c588
ad.hanif <ad.hanif@outlook.com>
2023-10-25 18:29:53 +0200

    created env file

--- pws-ui/src/store/authStore/useAuthStore.js
+++ pws-ui/src/store/authStore/useAuthStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { extractUserInfoFromToken } from "../../misc/tokenUtils";
 
 // const AUTH_API_URL = "http://localhost:8080/api/auth";
-const AUTH_API_URL = import.meta.env.VITE_BE_URL || "/api/auth";
+const AUTH_API_URL = (import.meta.env.VITE_BE_URL || "") + "/api/auth";
 
 // Create a Zustand store for authentication
 const useAuthStore = create((set) => {
