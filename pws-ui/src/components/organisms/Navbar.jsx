4e12b19bdf975d526320e4f79e6c11af0ad9deb3
ad.hanif <ad.hanif@outlook.com>
2023-10-29 13:59:50 +0100

    deleted all unused modules

--- pws-ui/src/components/organisms/Navbar.jsx
+++ pws-ui/src/components/organisms/Navbar.jsx
@@ -11,7 +11,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
 import { useNavigate, Link } from "react-router-dom";
 import useAuthStore from "../../store/authStore/useAuthStore.js";
