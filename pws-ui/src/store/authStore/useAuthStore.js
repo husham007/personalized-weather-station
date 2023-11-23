453086595f4d8d52e121b383b1b61c9bb672e74f
ad.hanif <ad.hanif@outlook.com>
2023-11-23 23:08:07 +0100

    rendering problem on refresh page in navbar fixed

--- pws-ui/src/store/authStore/useAuthStore.js
+++ pws-ui/src/store/authStore/useAuthStore.js
@@ -1,3 +1,66 @@
+import { create } from "zustand";
+import axiosClient from "./axiosClient";
+
+const useAuthStore = create((set) => ({
+  user: undefined,
+  isLoading: true,
+  setIsLoading: (isLoading) => {
+    set({ isLoading });
+  },
+  // Sign-in function
+  signIn: async (credentials) => {
+    try {
+      const response = await axiosClient.post("/signin", credentials);
+
+      set({ user: response.data, isLoading: false });
+
+      return { status: "success", message: "Sign-in successful!" };
+    } catch (error) {
+      return {
+        status: "error",
+        message: "Sign-in failed. Please check your credentials.",
+      };
+    }
+  },
+
+  signOut: async () => {
+    try {
+      const response = await axiosClient.post("/logout");
+      set((state) => {
+        return { user: undefined, isLoading: false };
+      });
+      return { success: true, message: "Logout successful!" };
+    } catch (error) {}
+  },
+
+  notification: {
+    open: false,
+    message: "",
+    severity: "",
+  },
+
+  setNotification: (open, message, severity) =>
+    set((state) => ({
+      notification: { ...state.notification, open, message, severity },
+    })),
+
+  getProfile: async () => {
+    try {
+      // set({ isLoading: false });
+
+      const response = await axiosClient.get(`/profile`);
+      set({ user: response.data });
+
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+}));
+
+export default useAuthStore;
+
+//
 // import { create } from "zustand";
 // import axios from "axios";
 // import { extractUserInfoFromToken } from "../../misc/tokenUtils";
@@ -94,72 +157,3 @@
 // });
 
 // export default useAuthStore;
-
-import { create } from "zustand";
-import axiosClient from "./axiosClient";
-
-const useAuthStore = create((set) => ({
-  user: undefined,
-  isLoading: undefined,
-
-  // Sign-in function
-  signIn: async (credentials) => {
-    try {
-      const response = await axiosClient.post("/signin", credentials);
-
-      set({ user: response.data, isLoading: true });
-
-      return { status: "success", message: "Sign-in successful!" };
-    } catch (error) {
-      return {
-        status: "error",
-        message: "Sign-in failed. Please check your credentials.",
-      };
-    }
-  },
-
-  signOut: async () => {
-    try {
-      const response = await axiosClient.post("/logout");
-
-      set((state) => {
-        return { user: undefined, isLoading: false };
-      });
-
-      return { success: true, message: "Logout successful!" };
-    } catch (error) {}
-  },
-
-  notification: {
-    open: false,
-    message: "",
-    severity: "",
-  },
-
-  setNotification: (open, message, severity) =>
-    set((state) => ({
-      notification: { ...state.notification, open, message, severity },
-    })),
-
-  getProfile: async () => {
-    try {
-      // set({ user: undefined, isLoading: false });
-      const response = await axiosClient.get(`/profile`);
-      set({ user: response.data});
-      return response.data;
-    } catch (error) {
-      console.log(error.response.status);
-    }
-  },
-
-  //   init: async () => {
-  //     try {
-  //       // Use getProfile to initialize user data
-  //       await useAuthStore.getState().getProfile();
-  //     } catch (error) {
-  //       console.error("Initialization failed:", error);
-  //     }
-  //   },
-}));
-
-export default useAuthStore;
