04905f0bb30abf0b4d010c5bcc678c1dd7ff5437
ad.hanif <ad.hanif@outlook.com>
2023-12-04 18:03:49 +0100

    credentials: include

--- pws-ui/src/axiosClient.js
+++ pws-ui/src/axiosClient.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosClient = axios.create({
   baseURL: (import.meta.env.VITE_BE_URL || "") + "/api/auth",
   withCredentials: true,
+  credentials: "include"
 });
 export default axiosClient;
 
